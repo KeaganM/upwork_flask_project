c8a96f886147792cdfd354ff54374fcfe59784ae
Johannes Fritz <fritz.johannes@gmail.com>
2020-10-01 22:43:17 +0200

    re-added ID's to main view + a person_list_view

--- migrations/2020_09_25_advance_data_view.sql
+++ migrations/2020_09_25_advance_data_view.sql
@@ -12,15 +12,17 @@ DROP VIEW IF EXISTS advance_data_view;
 -- proceeding_name
 -- speech date
 
-
 CREATE VIEW advance_data_view as 
 SELECT 
 speech_text,
 speech_date,
 word_count,
+speaker_list.speaker_id,
 first_name || ' ' || last_name as full_name,
+constituency_list.constituency_id,
 state_name,
 district_number,
+speaker_list.party_id,
 party_name,
 hearing_title
 -- speech_text,word_count
@@ -36,12 +38,18 @@ LEFT JOIN party_list on party_list.party_id = speaker_list.speaker_id
 LEFT JOIN hearing_speech on hearing_speech.speech_id = speech_list.speech_id
 LEFT JOIN hearing_list on hearing_list.hearing_id = hearing_speech.hearing_id;
 
-DROP VIEW IF EXISTS person_list_view;
+-- you need three helper views in this database:
+-- one each to back out the speaker_id, the constituency_id and the party_id. 
 
-CREATE VIEW person_list_view as
-SELECT first_name || ' ' || last_name as full_name
-FROM person_list;
+-- (1) helper view to back out the speaker ID; populate autcomplete from DISTINCT(display_name).
+CREATE VIEW person_list_view as 
+SELECT speaker_id, display_name
+FROM speaker_list sl
+JOIN person_list pl
+ON sl.person_id=pl.person_id;
 
+-- (2) helper view to back out the constituency ID:
+-- please use the table constituency_list and search through its state_name and district_number fields.
 
-
-
+-- (3) helper view to back out the party ID: 
+-- Not necessary, please reset the dropdown menu so that it passes the party ID through to the query (D=1, R=2, I=3).
