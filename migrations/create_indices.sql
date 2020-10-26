a7f4bd42115224a202c3e9b7ba68d3a5009485df
Keagan McNew <kmcnew2@gmail.com>
2020-10-26 16:24:23 -0500

    Initial Commit (feature)
    SQL used to create indices for tables/columns expected to be queried upon.

---
+++ migrations/create_indices.sql
@@ -0,0 +1,23 @@
+CREATE INDEX IF NOT EXISTS idx_person_display_dsc
+ON person_list(display_name DESC);
+
+CREATE INDEX IF NOT EXISTS idx_party_id
+ON party_list(party_id);
+
+CREATE INDEX IF NOT EXISTS idx_state_id
+ON constituency_list_normalized(state_id);
+
+CREATE INDEX IF NOT EXISTS idx_district_number
+ON constituency_list_normalized(district_id);
+
+CREATE INDEX IF NOT EXISTS idx_chamber_name
+ON chamber_list(chamber_name);
+
+CREATE INDEX IF NOT EXISTS idx_occasion_name
+ON occasion_list(occasion_name);
+
+CREATE INDEX IF NOT EXISTS idx_role_name
+ON person_role_list(person_role_name);
+
+CREATE INDEX IF NOT EXISTS idx_proceeding_name
+ON proceeding_list(proceeding_name);
