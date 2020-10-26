efa8e8a09c0c1b219cf1192c31876a453cc69a0c
Keagan McNew <kmcnew2@gmail.com>
2020-10-26 16:04:47 -0500

    Remove config.js (refactor)
    Remove config.js from main.js to allow both main.js and basic_query_form_component.js to share config variables.

--- static/js/main.js
+++ static/js/main.js
@@ -3,54 +3,71 @@
 // https://api.jquery.com/unwrap/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
 
-class QueryParameter {
-    constructor(conditional_type = "all", values = null) {
-        this.conditional_type = conditional_type
-        this.values = values
-        this.conditionals = this.conditional_type == "all" ? ["=", "!=", "<", ">", "<=", ">="] : ["=", "!="]
-    }
-}
-
-var districts = () => {
-    var districts = ["Choose a value"]
-    for (var i = 0; i <= 435; i++) {
-        districts.push(i)
-    }
-    return districts
-}
-
-
-var config = {
-    // field mapping for fields to select from
-    field_map: {
-        party_name: new QueryParameter("equate_only", ["Choose a value", "Democrat", "Republican", "Independent"]),
-        full_name: new QueryParameter("equate_only"),
-        word_count: new QueryParameter("all"),
-        district_number: new QueryParameter("equate_only", districts()),
-        state_name: new QueryParameter("equate_only", ["Choose a Value", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]),
-        chamber_name: new QueryParameter("equate_only", ["Choose a Value", "house", "joint", "senate"]),
-        occasion_name: new QueryParameter("equate_only", ["Choose a Value", "floor", "hearing"]),
-        role_name: new QueryParameter("equate_only", ["Choose a Value", "member", "chairman", "witness", "attendant"]),
-        proceeding_name: new QueryParameter("equate_only")
-    },
-    // jquery variable ids and classes
-    basic_date_id: "#basic_date_conditional",
-    basic_form_id: "#basic_form",
-    advance_form_id: "#advance_form",
-    column_options_id: "#option_form",
-    download_id: "#download",
-    display_query_checkbox: "#display_query_checkbox",
-    spinner_class: ".query-spinner",
-    advance_query_fieldset: ".advance-query-fieldset",
-
-    // html
-    new_table: "<table class='centered'><thead></thead><tbody></tbody></table>",
-
-
-    // general
-    row_num: 1
-
-}
+// class QueryParameter {
+//     constructor(conditional_type = "all", values = null, alias = null, in_basic_query_form = false, label = null, class_name = null) {
+//         this.conditional_type = conditional_type
+//         this.values = values
+//         this.alias = alias
+//         // this.temp_query_value = temp_querable_value
+//         this.conditionals = this.conditional_type == "all" ? ["=", "!=", "<", ">", "<=", ">="] : ["=", "!="]
+//
+//         // todo temp measure, will go back and redo front end in react
+//         this.in_basic_query_form = in_basic_query_form
+//         this.label = label
+//         this.class_name = class_name
+//
+//     }
+// }
+//
+// var states = ["Choose a Value", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+//
+// var districts = () => {
+//     var districts = ["Choose a value"]
+//     for (var i = 0; i <= 435; i++) {
+//         districts.push(i)
+//     }
+//     return districts
+// }
+//
+// var states_query_value = () => {
+//     var states_query_value = ["Choose a value"]
+//     for (var i = 1; i <= states.length; i++) {
+//         states_query_value.push(i)
+//     }
+//     return states_query_value
+// }
+//
+// var config = {
+//     // field mapping for fields to select from
+//     field_map: {
+//         display_name: new QueryParameter("equate_only", null, null, true, 'Full Name', 'autocomplete'),
+//         state_id: new QueryParameter("equate_only", states_query_value(), states, true, 'State Name'),
+//         district_number: new QueryParameter("equate_only", districts(), null, true, 'District'),
+//         party_id: new QueryParameter("equate_only", ["Choose a value", 1, 2, 3, 4], ["Choose a value", "Democrat", "Republican", "Independent", "Undeclared"], true, 'Party'),
+//         word_count: new QueryParameter("all"),
+//         chamber_name: new QueryParameter("equate_only", ["Choose a Value", "house", "joint", "senate"]),
+//         occasion_name: new QueryParameter("equate_only", ["Choose a Value", "floor", "hearing"]),
+//         role_name: new QueryParameter("equate_only", ["Choose a Value", "member", "chairman", "witness", "attendant"]),
+//         proceeding_name: new QueryParameter("equate_only")
+//     },
+//     // jquery variable ids and classes
+//     basic_date_id: "#basic_date_conditional",
+//     basic_form_id: "#basic_form",
+//     advance_form_id: "#advance_form",
+//     column_options_id: "#option_form",
+//     download_id: "#download",
+//     display_query_checkbox: "#display_query_checkbox",
+//     spinner_class: ".query-spinner",
+//     advance_query_fieldset: ".advance-query-fieldset",
+//
+//     // html
+//     new_table: "<table class='centered'><thead></thead><tbody></tbody></table>",
+//
+//
+//     // general
+//     row_num: 1
+//
+// }
 
 var $query_spinner = $(config.spinner_class);
 var $download = $(config.download_id);
@@ -62,6 +79,10 @@ var $advance_query_fieldset_html = $(config.advance_query_fieldset)
     .html()
 
 // function ************************************************************************************************************
+
+
+
+
 
 function update_autocomplete(element) {
     element.bind('input', function () {
@@ -112,10 +133,10 @@ function advance_form_value_factory(element_value, field_map) {
     var user_input_type = Array.isArray(field_map[element_value].values) ? "select" : "text";
     var condition_html = create_conditional_field(field_map[element_value].conditionals);
     var html = element_value == "full_name" ? "<input id='value' class='autocomplete' name='value' type='text' value=''>" : "<input id='value' name='value' type='text' value=''>"
-
+// todo set the alias to either be the alias set in the queryparamter object or as values to fix the below
     if (user_input_type == "select") {
         html = "<select id='value' name='value'>"
-        for (var i = 0; i < field_map[element_value]["values"].length; i++) {
+        for (var i = 0; i < field_map[element_value].values.length; i++) {
             html = html + "<option value='" + field_map[element_value].values[i] + "'>" + field_map[element_value].values[i] + "</option>"
         }
         html = html + "</select>"
@@ -158,7 +179,7 @@ function request(url, data, type = "GET", contentType = "application/json; chars
         type: type,
         url: url,
         data: data,
-        contentType, contentType,
+        contentType: contentType,
         success: function (response) {
             callback(response)
         }
@@ -171,6 +192,9 @@ function Query() {
 
     var values = get_query_inputs(config.basic_form_id, "basic")
     values.push(...get_query_inputs(config.advance_form_id, "advance"))
+
+    console.log('values are:')
+    console.log(values)
 
     var data = {
         data: {
@@ -228,7 +252,7 @@ function Query() {
             if (data == 'no records found') {
                 //TODO make alert box pretty
                 // alert("no records found")
-                M.toast({html:'No records found'})
+                M.toast({html: 'No records found'})
                 reset_elements([$("#after_query_message").find('p'), $('#results-found').find('p')],
                     ['<p></p>', '<p></p>'])
 
@@ -328,6 +352,8 @@ $(document).ready(function () {
 
     $query_spinner.hide();
 
+    // create_basic_query_form(config.field_map)
+
 
     // this is for date picker; it does have paramaters
     $(".datepicker").datepicker({
@@ -349,50 +375,5 @@ $(document).ready(function () {
 
     // get names from api and create autocomplee
     // https://gist.github.com/brandonaaskov/1596867
-    update_autocomplete($('#full_name'))
+    update_autocomplete($('#display_name'))
 });
-
-
-// ******************* tests
-
-
-// ********************* go through later
-
-// $(document).on({
-//     ajaxStart:function(){$query_spinner.attr("display","block");},
-//     ajaxStop:function(){$query_spinner.attr("display","none");}
-// });
-
-// show/hide advance query
-//$(document).on("click",".advance-query-button",function(){
-//    var $advance_query_div = $(".advance-query-fieldset")
-//    var $advance_query_div_display = $advance_query_div.css("display")
-//    if ($advance_query_div_display == "none"){
-//        $advance_query_div.css("display","block")
-//    }else{
-//        $advance_query_div.css("display","none")
-//    }
-//})
-
-//$(document).ready(function(){
-//    $("#advance_query > div:nth-child(2) > select").change(function(){
-//        var val = $(this).val();
-//        html = advance_form_value_factory(val,field_map)
-//        $("#value").replaceWith(html)
-//    })
-//})
-
-
-//    $(document).on("change","#"+new_id+"> div:nth-child(2) > select",function(){
-//        var val = $(this).val();
-//        console.log(val)
-//        html = advance_form_value_factory(val,field_map)
-//        $("#"+new_id+" > div:nth-child(4) > input").replaceWith(html)
-//
-//    })
-
-
-//    var $advance_query_div_elements = $advance_query_div.children()
-//    .find("select").end()
-//    .find("input").end()
-//    console.log($advance_query_div_elements)
