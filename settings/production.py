7d055c3fc9a93811b9a3373e8c4a8a02d8e8eb87
Keagan McNew <kmcnew2@gmail.com>
2020-10-26 16:08:06 -0500

    Remove base.py import and change variable assignment (refactor)
    Remove base.py import and remove db assignment in favor of assignment to SQLALCHEMY_DATABASE_URI  and changed application.config["SECRET_KEY"] assignment to SECRET_KEY. Both will be imported to in config.py and assigned appropriately.

--- settings/production.py
+++ settings/production.py
@@ -1,9 +1,8 @@
-from settings.base import *
 import secrets
 import os
 
-application.config["SECRET_KEY"] = secrets.token_hex(16)
-db = Database(path=os.environ['RR_DATABASE'], create_connection=False)
-
+SECRET_KEY = secrets.token_hex(16)
+# db = Database(path=os.environ['RR_DATABASE'], create_connection=False)
+SQLALCHEMY_DATABASE_URI = os.environ['RR_DATABASE']
 DEBUG = False
 
