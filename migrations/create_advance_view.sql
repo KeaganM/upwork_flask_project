5cb832bfd32fbae25dbf06f68590ac0b1f1b60fa
Keagan McNew <kmcnew2@gmail.com>
2020-10-26 16:21:37 -0500

    Initial Commit (feature)
    SQL used to create a consituency_list helper view and an advance_data view which is used to in the query of the database. Changed some joins to be inner joins, added consituency_list_view to pull states and districts together in relation to person id.
    
    Added columns party_id,state_id,district_id to be used in the query rather than
    their string counterparts.

---
+++ migrations/create_advance_view.sql
@@ -0,0 +1,53 @@
+-- create constituency_list_normalized view
+DROP VIEW IF EXISTS constituency_list_normalized_view;
+
+CREATE VIEW constituency_list_normalized_view as 
+SELECT constituency_list_normalized.constituency_id,constituency_list_normalized.state_id,state_name,constituency_list_normalized.district_id,district_number
+FROM constituency_list_normalized
+INNER JOIN states_list on states_list.state_id = constituency_list_normalized.state_id
+INNER JOIN district_list on district_list.district_id = constituency_list_normalized.district_id;
+
+SELECT * FROM constituency_list_normalized_view;
+
+-- create overall data view
+DROP VIEW IF EXISTS advance_data_view2;
+
+-- want columns:
+-- speech_text
+-- word_count
+-- full_name
+-- state_name
+-- district_number
+-- heading_title
+-- party name
+-- chamber_name
+-- proceeding_name
+-- speech date
+
+CREATE VIEW advance_data_view2 as 
+SELECT 
+speech_text,
+speech_date,
+word_count,
+display_name,
+constituency_list_normalized_view.state_id,
+state_name,
+constituency_list_normalized_view.district_id,
+district_number,
+speaker_list.party_id,
+party_name,
+occasion_name,
+person_role_name,
+chamber_name,
+proceeding_name
+FROM speech_list
+INNER JOIN speaker_list on speaker_list.speaker_id = speech_list.speaker_id
+INNER JOIN person_list on person_list.person_id = speaker_list.person_id
+INNER JOIN party_list on party_list.party_id = speaker_list.party_id
+INNER JOIN occasion_list on occasion_list.occasion_id = speech_list.occasion_id
+LEFT JOIN speech_proceeding on speech_proceeding.speech_id = speech_list.speech_id
+LEFT JOIN proceeding_list on proceeding_list.proceeding_id = speech_proceeding.proceeding_id
+INNER JOIN chamber_list on chamber_list.chamber_id = speaker_list.chamber_id
+INNER JOIN person_role_list on person_role_list.person_role_id = speech_list.person_role_id
+LEFT JOIN constituency_list_normalized_view on constituency_list_normalized_view.constituency_id = speaker_list.constituency_id;
+
