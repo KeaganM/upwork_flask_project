a183f40e5cd7041af8a95f80668328c73dd6971c
Keagan McNew <kmcnew2@gmail.com>
2020-10-26 21:49:16 -0500

    Remove commented out code (refactor)

--- settings/production.py
+++ settings/production.py
@@ -2,7 +2,6 @@ import secrets
 import os
 
 SECRET_KEY = secrets.token_hex(16)
-# db = Database(path=os.environ['RR_DATABASE'], create_connection=False)
 SQLALCHEMY_DATABASE_URI = os.environ['RR_DATABASE']
 DEBUG = False
 
